48625426a9201bcd7159e7a238c7978967a04327
Lorenzo Tabasso <lorenzo.tabasso96@gmail.com>
2020-05-21 12:54:05 +0200

    All three sentences works.

--- part1/src/translator.py
+++ part1/src/translator.py
@@ -1,15 +1,14 @@
-from nltk import parse
+import nltk
 import sys
 
 
 def main():
-    my_parser = parse.load_parser("../grammars/my-simple-sem.fcfg")
-    # sentence0 = "Angus gives a bone to every dog"
+    my_parser = nltk.parse.load_parser("../grammars/my-simple-sem.fcfg")
+
     sentence1 = "You are imagining things"
-    sentence2 = "There is a price on my head"  # TODO: controllare grammatica
-    # sentence3 = "Your big opportunity is flying out of here"
-    # tokens = [sentence1.split(), sentence2.split(), sentence3.split()]
-    tokens = [sentence1.split(), sentence2.split()]
+    sentence2 = "There is a price on my head"
+    sentence3 = "Your big opportunity is flying out of here"
+    tokens = [sentence1.split(), sentence2.split(), sentence3.split()]
 
     for sentences in tokens:
         for tree in my_parser.parse(sentences):
