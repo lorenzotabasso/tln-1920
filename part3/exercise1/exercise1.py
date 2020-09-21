208865dcd5e6971fd6a2b8e43332fea8887bc6e6
VittorioParagallo <vittorio.paragall@edu.unito.it>
2020-09-21 19:17:38 +0200

    ptima revisione primo esercizio

--- part3/exercise1/exercise1.py
+++ part3/exercise1/exercise1.py
@@ -1,8 +1,26 @@
 import csv
 import nltk
+from nltk import word_tokenize
 from nltk.corpus import stopwords
 import numpy as np
 import pandas as pd
+
+"""
+In case the nltk stopwords download fails run from python3 cli:
+
+import nltk
+import ssl 
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download()
+
+"""
 
 
 def load_data():
@@ -45,7 +63,8 @@ def preprocess(definition):
     punct = {',', ';', '(', ')', '{', '}', ':', '?', '!', '.'}
     wnl = nltk.WordNetLemmatizer()
     tokens = nltk.word_tokenize(definition)
-    tokens = list(filter(lambda x: x not in stop_words and x not in punct, tokens))
+    tokens = list(
+        filter(lambda x: x not in stop_words and x not in punct, tokens))
 
     # Lemmatization
     lemmatized_tokens = set(wnl.lemmatize(t) for t in tokens)
@@ -53,9 +72,9 @@ def preprocess(definition):
     return lemmatized_tokens
 
 
-def compute_overlap(definitions):
+def compute_overlap_terms(definitions):
     """
-    It computes the overlap between the two set of the preprocessed definitions
+    It computes the overlap between the two set of the preprocessed terms
     :param definitions: a list of definitions (strings)
     :return: a list of length pow(len(definition)) containing the similarity
     score of each definition.
@@ -64,17 +83,14 @@ def compute_overlap(definitions):
     # list of similarity score for each type of definition (length: pow(len(definition)))
     results = []
 
-    for d1 in definitions:
+    for j, d1 in enumerate(definitions, start=0):
         a = preprocess(d1)  # set of terms of the first definition
-        for d2 in definitions:
-            b = preprocess(d2)  # set of terms of the second definition
-
-            # Computing similarity between definitions
-            t = len(a & b) / min(len(a), len(b))
-
-            if not t == 1.0:  # A and B are the same
+        if not j < len(definitions) - 1:
+            for k, d2 in enumerate(definitions, start=j+1):
+                b = preprocess(d2)  # set of terms of the second definition
+                # Computing similarity between definitions
+                t = len(a & b) / min(len(a), len(b))
                 results.append(t)
-
     # DEBUG
     # mean = np.mean(results)
     # print("MEAN: {} - RESULTS: {}".format(mean, results))
@@ -82,29 +98,80 @@ def compute_overlap(definitions):
     return results
 
 
-def compute_overlap_max(definitions):
+# def compute_overlap_max(definitions):
+#     """
+#     It computes the overlap between the two set of the preprocessed definitions
+#     :param definitions: a list of definitions (strings)
+#     :return: a list of length |definitions| containing the maximum similarity
+#     score of each definition.
+#     """
+#     max_value = 0
+#     # list of the best similarity score for each type of definition (length: len(definitions))
+#     results = []
+
+#     for d1 in definitions:
+#         a = preprocess(d1)  # set of terms of the first definition
+#         for d2 in definitions:
+#             b = preprocess(d2)  # set of terms of the second definition
+
+#             # Computing similarity between definitions
+#             t = len(a & b) / min(len(a), len(b))
+
+#             if not t == 1.0 and t > max_value:
+#                 max_value = t
+#         results.append(max_value)
+#         max_value = 0
+
+#     # DEBUG
+#     # mean = np.mean(results)
+#     # print("MEAN: {} - RESULTS: {}".format(mean, results))
+
+#     return results
+
+def compute_overlap_pos(definitions):
     """
-    It computes the overlap between the two set of the preprocessed definitions
+    It computes the overlap between the two set of the preprocessed definitions 
+    converted in POS tagging.
     :param definitions: a list of definitions (strings)
     :return: a list of length |definitions| containing the maximum similarity
     score of each definition.
     """
-    max_value = 0
-    # list of the best similarity score for each type of definition (length: len(definitions))
+
     results = []
 
-    for d1 in definitions:
-        a = preprocess(d1)  # set of terms of the first definition
-        for d2 in definitions:
-            b = preprocess(d2)  # set of terms of the second definition
+    # >>> text = word_tokenize("And now for something completely different")
+    # >>> nltk.pos_tag(text)
+    # [('And', 'CC'), ('now', 'RB'), ('for', 'IN'), ('something', 'NN'),
+    # ('completely', 'RB'), ('different', 'JJ')]
 
-            # Computing similarity between definitions
-            t = len(a & b) / min(len(a), len(b))
+    for j, d1 in enumerate(definitions, start=0):
+        text1 = word_tokenize(d1)
+        temp_a = nltk.pos_tag(text1)
+        print("AAAAAAAAAAAAAA")
+        print(j, temp_a)
+        
+        # x = map(myfunc, ('apple', 'banana', 'cherry'))
+        #a = [lambda x: x in temp_a[1]]
+        a = [x for x in temp_a[1]]
+        print(j, a)
 
-            if not t == 1.0 and t > max_value:
-                max_value = t
-        results.append(max_value)
-        max_value = 0
+        # if not j < len(definitions) - 1:
+        #     for k, d2 in enumerate(definitions, start=j+1):
+        #         text2 = word_tokenize(d2)
+        #         temp_b = nltk.pos_tag(text2)
+        #         print("BBBBBBBBBBB")
+        #         # print(k, temp_b)
+        #         b = [y for y in temp_b[1]]
+        #         print(b)
+
+                # Computing similarity between definitions
+                #intersec = [value for value in a if value in b]
+                #list(filter(lambda x: x not in stop_words and x not in punct, tokens))
+                # intersec = [lambda z: z in a and z in b]
+                # t = len(intersec) / min(len(a), len(b))  # normalization step
+                # print("TEST {} {} - LA {} LB - {} || {} {}".format(intersec,
+                #                                                    t, len(a), len(b), a, b))
+                # results.append(t)
 
     # DEBUG
     # mean = np.mean(results)
@@ -116,7 +183,7 @@ def compute_overlap_max(definitions):
 if __name__ == "__main__":
 
     options = {
-        "output": "/Users/lorenzotabasso/Desktop/University/TLN/Progetto/19-20/tln-1920/part3/exercise1/input/",
+        "output": "./part3/exercise1/input/"
     }
 
     defs = load_data()  # Loading the definitions.csv file
@@ -129,10 +196,10 @@ if __name__ == "__main__":
 
     for d in defs:
         # computing the mean of the overlap of the definitions
-        overlap = compute_overlap(d)
+        overlap = compute_overlap_terms(d)
         mean = np.mean(overlap)
 
-        overlap_max = compute_overlap_max(d)
+        overlap_max = compute_overlap_pos(d)
         mean_max = np.mean(overlap_max)
 
         # making the percentage of the mean
@@ -158,9 +225,9 @@ if __name__ == "__main__":
     df = pd.DataFrame([first_row, second_row], columns=["Abstract", "Concrete"],
                       index=["Generic", "Specific"])
     df_max = pd.DataFrame([third_row, fourth_row], columns=["Abstract", "Concrete"],
-                      index=["Generic", "Specific"])
+                          index=["Generic", "Specific"])
     print(df)
-    print("\nMax experiment:\n")
+    print("\nPOS experiment:\n")
     print(df_max)
 
     # TODO: Make report.
